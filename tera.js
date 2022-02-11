8af5ab04f3087668b4daf747ed33b7fde2cd94dd
LightningWasntTaken <85737041+LightningWasntTaken@users.noreply.github.com>
2022-02-11 13:20:21 +0530

    Add files via upload

--- tera.js
+++ tera.js
@@ -1,6 +1,27 @@
-function addUser() {
-    user_name = document.getElementById("user_name_input").value;
-    localStorage.setItem("user_name_input" , user_name);
-    window.location = "tera_room.html";
+var firebaseConfig = {
+    apiKey: "AIzaSyDNVlGlHR6oYeFJKyBniw4NgZrsMUSFa38",
+    authDomain: "practise-activity-c7d2c.firebaseapp.com",
+    databaseURL: "https://practise-activity-c7d2c-default-rtdb.firebaseio.com",
+    projectId: "practise-activity-c7d2c",
+    storageBucket: "practise-activity-c7d2c.appspot.com",
+    messagingSenderId: "988588423363",
+    appId: "1:988588423363:web:2c3d8b3eea4db5eb368a76"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
+function addUser() {
+    user_name_tera = document.getElementById("user_name_input").value;
+    if(user_name_tera == ""){
+        document.getElementById("user_name_input").placeholder = "Please enter your username";
+    }
+    else{
+        localStorage.setItem("user_name" , user_name_tera);
+    window.location = "tera_room.html";
+    }
+    firebase.database().ref("/").child(user_name_tera).update({
+        purpose: "adding username"
+    });
 }
+
--- tera_room.js
+++ tera_room.js
@@ -1,13 +1,15 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyAyT8lOdMPV8psEdgMvfs17f6SmASGS8Kg",
-    authDomain: "class-test-2fa45.firebaseapp.com",
-    projectId: "class-test-2fa45",
-    storageBucket: "class-test-2fa45.appspot.com",
-    messagingSenderId: "402035720175",
-    appId: "1:402035720175:web:233794660599f6e889001a"
-  };
+var firebaseConfig = {
+  apiKey: "AIzaSyDNVlGlHR6oYeFJKyBniw4NgZrsMUSFa38",
+  authDomain: "practise-activity-c7d2c.firebaseapp.com",
+  databaseURL: "https://practise-activity-c7d2c-default-rtdb.firebaseio.com",
+  projectId: "practise-activity-c7d2c",
+  storageBucket: "practise-activity-c7d2c.appspot.com",
+  messagingSenderId: "988588423363",
+  appId: "1:988588423363:web:2c3d8b3eea4db5eb368a76"
+};
 
-  const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
   function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
     Room_names = childKey;
@@ -16,3 +18,19 @@ const firebaseConfig = {
 
 });});}
 getData();
+
+function logout(){
+  localStorage.removeItem("user_name");
+  localStorage.removeItem("room_name");
+
+  window.location = "index.html";
+}
+
+function addRoom() {
+  room_name = document.getElementById("room_name").value;
+  firebase.database().ref("/").child(room_name).update({
+    purpose: "Adding room name"
+  });
+  localStorage.setItem("room_name" , room_name);
+  window.location = "tera_chat.html";
+}
